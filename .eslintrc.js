692a2a60598cea86e674bc4b42dfe945a084c354
Sankalp Sharma <sansharma056@gmail.com>
2020-12-13 23:13:58 +0530

    Added conf files

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  parserOptions: {
+    sourceType: "module",
+  },
+  parser: "babel-eslint",
+  env: {
+    node: true,
+  },
+  extends: ["standard", "prettier", "prettier/standard"],
+  plugins: ["prettier"],
+};
