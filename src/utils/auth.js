5faac6fd7d7d3f10cccaec8cceffd08f91315268
Sankalp Sharma <sansharma056@gmail.com>
2020-12-20 03:59:37 +0530

    Added screenName to the payload

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -3,9 +3,13 @@ import { User } from "../resources/user/user.model";
 import jwt from "jsonwebtoken";
 
 export const newToken = (user) => {
-  return jwt.sign({ id: user.id }, config.secrets.jwt, {
-    expiresIn: config.secrets.jwtExp,
-  });
+  return jwt.sign(
+    { id: user.id, screenName: user.screen_name },
+    config.secrets.jwt,
+    {
+      expiresIn: config.secrets.jwtExp,
+    }
+  );
 };
 
 export const verifyToken = (token) => {
@@ -22,7 +26,7 @@ export const signup = async (req, res) => {
     !req.body.email ||
     !req.body.screen_name ||
     !req.body.name ||
-    !req.body.dob ||
+    !req.body.date_of_birth ||
     !req.body.password
   ) {
     return res
@@ -52,7 +56,7 @@ export const signin = async (req, res) => {
 
   try {
     const user = await User.findOne({ email: req.body.email })
-      .select("email password")
+      .select("screen_name password")
       .exec();
     if (!user) {
       return res.status(400).send(invalid);
@@ -76,7 +80,9 @@ export const protect = async (req, res, next) => {
   const bearer = req.headers.authorization;
 
   if (!bearer || !bearer.startsWith("Bearer ")) {
-    return res.status(401).end();
+    return res
+      .status(401)
+      .send({ message: "Something went wrong. Please try again." });
   }
 
   const token = bearer.split("Bearer ")[1];
