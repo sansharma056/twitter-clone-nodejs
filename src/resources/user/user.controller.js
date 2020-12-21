d6df91aecdfa24f45bf5b17ff48dcf75fb5a85af
Sankalp Sharma <sansharma056@gmail.com>
2020-12-21 21:20:15 +0530

    Manipulating userData before sending response

--- src/resources/user/user.controller.js
+++ src/resources/user/user.controller.js
@@ -2,18 +2,32 @@ import { User } from "./user.model";
 
 export const getOne = async (req, res) => {
   try {
-    const user = await User.findOne({ screen_name: req.params.screenName })
+    const userData = await User.findOne({ screen_name: req.params.screenName })
       .select("-password")
       .lean()
       .exec();
 
-    if (!user) {
+    if (!userData) {
       return res.status(404).send({
         message: "This account doesn’t exist\nTry searching for another.",
       });
     }
 
-    user.isSelf = req.user._id === user._id;
+    const user = {
+      avatarURL: userData.profile_picture_url,
+      bannerURL: userData.banner_url,
+      name: userData.name,
+      handle: userData.screen_name,
+      bio: userData.description,
+      location: userData.location,
+      website: userData.url,
+      birthDate: userData.date_of_birth,
+      joinDate: userData.createdAt,
+      followers: userData.followers_list,
+      following: userData.following_list,
+      tweets: userData.tweets_list,
+      isSelf: req.user._id === userData._id,
+    };
 
     res.status(200).send({ user });
   } catch (e) {
@@ -21,3 +35,5 @@ export const getOne = async (req, res) => {
     res.status(400).end();
   }
 };
+
+export const updateOne = async (req, res) => {};
