55a148b22ff3ebb1c7502d286e9d5b88cdbaa141
Sankalp Sharma <sansharma056@gmail.com>
2020-12-23 23:22:34 +0530

    Added options which remove the use of deprecated functions

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,6 +1,14 @@
 import mongoose from "mongoose";
 import config from "../config";
 
-export const connect = (url = config.dbURL, opts = {}) => {
+export const connect = (
+  url = config.dbURL,
+  opts = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }
+) => {
   return mongoose.connect(url, opts);
 };
