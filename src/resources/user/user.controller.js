6da32bcd692f66124b63502242d21ab0f085a01c
Sankalp Sharma <sansharma056@gmail.com>
2020-12-26 22:38:06 +0530

    Using cloudinary to store images

--- src/resources/user/user.controller.js
+++ src/resources/user/user.controller.js
@@ -1,7 +1,7 @@
 import { User } from "./user.model";
-import fs from "fs";
-import path from "path";
 import validator from "validator";
+import cloudinary from "cloudinary";
+import config from "../../config/";
 
 export const getOne = async (req, res) => {
   try {
@@ -41,7 +41,6 @@ export const getOne = async (req, res) => {
 
 export const updateOne = async (req, res) => {
   try {
-    const basePath = path.join(__dirname, "../../");
     const profile_media_urls = {};
 
     if (
@@ -50,78 +49,41 @@ export const updateOne = async (req, res) => {
         req.body.profile_picture_url.replace(/^data:([A-Za-z-+/]+);base64,/, "")
       )
     ) {
-      const avatarPath = path.join(
-        basePath,
-        `public/profile_images/${req.user.screen_name}/`
-      );
-
-      const avatarFile = `${Date.now()}.png`;
-
-      fs.mkdir(
-        avatarPath,
-        {
-          recursive: true,
-        },
-        (error) => {
-          if (error) {
-            console.log(error);
-          } else {
-            fs.writeFile(
-              avatarPath + avatarFile,
-              req.body.profile_picture_url.replace(
-                /^data:([A-Za-z-+/]+);base64,/,
-                ""
-              ),
-              { encoding: "base64" },
-              (error) => {
-                if (error) {
-                  console.log(error);
-                }
-              }
-            );
-          }
-        }
-      );
-
-      profile_media_urls.profile_picture_url = `http://localhost:3000/profile_images/${req.user.screen_name}/${avatarFile}`;
+      cloudinary.v2.config(config.cloudinary);
+      await cloudinary.v2.uploader
+        .upload(req.body.profile_picture_url, {
+          folder: "profile_images",
+          unique_filename: true,
+          discard_original_filename: true,
+          allowed_formats: ["png"],
+        })
+        .then((result) => {
+          profile_media_urls.profile_picture_url = result.secure_url;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-
     if (
       req.body.banner_url &&
       validator.isBase64(
         req.body.banner_url.replace(/^data:([A-Za-z-+/]+);base64,/, "")
       )
     ) {
-      const bannerPath = path.join(
-        basePath,
-        `public/profile_banners/${req.user.screen_name}/`
-      );
-
-      const bannerFile = `${Date.now()}.png`;
-
-      fs.mkdir(
-        bannerPath,
-        {
-          recursive: true,
-        },
-        (error) => {
-          if (error) {
-            console.log(error);
-          } else {
-            fs.writeFile(
-              bannerPath + bannerFile,
-              req.body.banner_url.replace(/^data:([A-Za-z-+/]+);base64,/, ""),
-              { encoding: "base64" },
-              (error) => {
-                if (error) {
-                  console.log(error);
-                }
-              }
-            );
-          }
-        }
-      );
-      profile_media_urls.banner_url = `http://localhost:3000/profile_banners/${req.user.screen_name}/${bannerFile}`;
+      cloudinary.v2.config(config.cloudinary);
+      await cloudinary.v2.uploader
+        .upload(req.body.banner_url, {
+          folder: "profile_banners",
+          unique_filename: true,
+          discard_original_filename: true,
+          allowed_formats: ["png"],
+        })
+        .then((result) => {
+          profile_media_urls.banner_url = result.secure_url;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }
     const updatedUser = await User.findOneAndUpdate(
       { screen_name: req.user.screen_name },
@@ -131,6 +93,7 @@ export const updateOne = async (req, res) => {
       },
       { new: true }
     );
+    res.status(200).send({ message: "Profile edited." });
   } catch (error) {
     console.log(error);
     res.status(400).send({ message: "Something went wrong.Please try again." });
