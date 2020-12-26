ae2dec401d7b23327f7e44d5b08344c990456f51
Sankalp Sharma <sansharma056@gmail.com>
2020-12-26 18:43:47 +0530

    Added API configuration

--- src/config/dev.js
+++ src/config/dev.js
@@ -3,4 +3,9 @@ export const config = {
     jwt: "devsecret",
   },
   dbURL: "mongodb://localhost:27017/twitter-clone",
+  cloudinary: {
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+  },
 };
