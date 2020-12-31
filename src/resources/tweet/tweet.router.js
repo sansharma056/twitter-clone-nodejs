2507e2ecc8945069018ba8ffbe9fd5ce9b6efc48
Sankalp Sharma <sansharma056@gmail.com>
2020-12-31 23:17:45 +0530

    Added new routes

--- src/resources/tweet/tweet.router.js
+++ src/resources/tweet/tweet.router.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createOne, getOne } from "./tweet.controller";
+import { createOne, deleteOne, getOne, getMany } from "./tweet.controller";
 
 const router = Router();
 
 router.post("/", createOne);
+router.get("/", getMany);
 router.get("/:id", getOne);
+router.delete("/:id", deleteOne);
 
 export default router;
