19e45fb081c78fba407c085be737deb78696f4ea
Sankalp Sharma <sansharma056@gmail.com>
2021-01-03 21:39:21 +0530

    Updated Tweet model

--- src/resources/tweet/tweet.model.js
+++ src/resources/tweet/tweet.model.js
@@ -10,8 +10,9 @@ const tweetSchema = new mongoose.Schema(
     media_url: {
       type: String,
     },
-    in_repy_to_screen_name: {
-      type: String,
+    in_repy_to_user: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "user",
     },
     in_reply_to_status_id: {
       type: mongoose.SchemaTypes.ObjectId,
@@ -28,11 +29,23 @@ const tweetSchema = new mongoose.Schema(
     favorited_by_list: {
       type: [{ type: mongoose.SchemaTypes.ObjectId, ref: "user" }],
     },
-    replies_list: {
+    statuses_list: {
       type: [{ type: mongoose.SchemaTypes.ObjectId, ref: "tweet" }],
     },
   },
   { timestamps: true }
 );
+
+tweetSchema.virtual("retweet_count").get(function () {
+  return this.retweeted_by_list.length;
+});
+
+tweetSchema.virtual("favorites_count").get(function () {
+  return this.favorited_by_list.length;
+});
+
+tweetSchema.virtual("statuses_count").get(function () {
+  return this.replies_by_list.length;
+});
 
 export const Tweet = mongoose.model("tweet", tweetSchema);
