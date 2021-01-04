db347c29d81d150754440a3d355343baf05bff6c
Sankalp Sharma <sansharma056@gmail.com>
2021-01-04 18:41:57 +0530

    Added new routes

--- src/resources/tweet/tweet.router.js
+++ src/resources/tweet/tweet.router.js
@@ -1,11 +1,22 @@
 import { Router } from "express";
-import { createOne, deleteOne, getOne, getMany } from "./tweet.controller";
+import {
+  createOne,
+  deleteOne,
+  getOne,
+  getMany,
+  validateTweetId,
+} from "./tweet.controller";
+import favoriteRouter from "./favorite/favorite.router";
 
 const router = Router();
 
 router.post("/", createOne);
+
+router.use("/:id", validateTweetId);
+
 router.get("/", getMany);
 router.get("/:id", getOne);
+router.use("/:id/favorite", favoriteRouter);
 router.delete("/:id", deleteOne);
 
 export default router;
