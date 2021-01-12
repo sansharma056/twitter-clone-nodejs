bcae71898abf745cab9ec87f25bdbc00060db255
Sankalp Sharma <sansharma056@gmail.com>
2021-01-12 20:46:56 +0530

    Added followRouter

--- src/resources/user/user.router.js
+++ src/resources/user/user.router.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { getOne, updateOne } from "./user.controller";
+import followRouter from "./follow/follow.router";
 
 const router = Router();
 
 router.get("/:screenName", getOne);
 router.put("/:screenName", updateOne);
+
+router.use("/:screenName/follow", followRouter);
+
 export default router;
