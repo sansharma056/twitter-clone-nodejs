76d7bcb70e3c8ff4d623e6cc4be5d946ee8d5422
Sankalp Sharma <sansharma056@gmail.com>
2021-03-15 21:45:08 +0530

    Added new routes

--- src/resources/tweet/tweet.router.js
+++ src/resources/tweet/tweet.router.js
@@ -8,6 +8,8 @@ import {
 } from "./tweet.controller";
 import favoriteRouter from "./favorite/favorite.router";
 import retweetRouter from "./retweet/retweet.router";
+import bookmarkRouter from "./bookmark/bookmark.router";
+
 const router = Router();
 
 router.post("/", createOne);
@@ -20,5 +22,6 @@ router.delete("/:id", deleteOne);
 
 router.use("/:id/favorite", favoriteRouter);
 router.use("/:id/retweet", retweetRouter);
+router.use("/:id/bookmark", bookmarkRouter);
 
 export default router;
